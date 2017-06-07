4d2e646466563888e2b85652585a9a91b9928303
Ayush Bhat <tfeayush@gmail.com>
2017-06-07 21:00:42 +0530

    Add files via upload

---
+++ TopCoder/array_hash.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int alpha(char ch)
+{
+  if(ch > 90)
+    {
+      return ch-97;
+    }
+  else return ch - 65;
+}
+
+int sum(char **str, int n)
+{
+  int t = 0;
+  int sum=0;
+  char ch = ' ';
+  for(int i = 0; i < n; i++)
+    {
+      while(ch != '\0')
+	{
+	  sum = sum + t + i + alpha(ch);
+	  ch = *(str[i] + t);
+	  t++;
+	}
+      ch = ' ';
+      t = 0;
+    }
+  return sum;
+}
+
+int main()
+{
+  int n;
+  printf("Enter the number of strings: ");
+  scanf("%d", &n);
+  char * ch [n];
+  for(int i = 0; i < n; i ++)
+    {
+      ch[i]  =  (char *) malloc(sizeof(char *)*1000);
+      scanf("%s", ch[i]);
+    }
+  int sum1 = sum(ch, n);
+  printf("%d", sum1);
+  return 0;
+}
